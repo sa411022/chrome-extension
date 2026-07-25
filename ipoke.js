cbc0ec95c652fa242e87b96fa65dcfb49e7475dc
sa411022 <sa411022@gmail.com>
2026-07-25 15:58:14 +0800

    Create ipoke.js

---
+++ ipoke.js
@@ -0,0 +1,12 @@
+interval = setInterval(() => {
+  if(document.body.querySelector("div.fc-monetization-dialog-container")) {
+    document.body.querySelector("div.fc-monetization-dialog-container").style.setProperty("display", "none", "important");
+    clearInterval(interval);
+  }
+}, 1000);
+
+const link = document.createElement("link");
+link.rel = "icon";
+link.type = "image/png";
+link.href = chrome.runtime.getURL("ipoke.png");
+document.head.appendChild(link);
