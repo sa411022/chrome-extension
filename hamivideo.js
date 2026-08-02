3c5b3697acb805b7e47e274414e0f81d104d627f
sa411022 <sa411022@gmail.com>
2026-08-02 14:09:00 +0800

    Update hamivideo.js

--- hamivideo.js
+++ hamivideo.js
@@ -1,23 +1,5 @@
-document.addEventListener("fullscreenchange", () => {
-  const video = document.body.querySelector("video#h5video");
-  const scale = Math.min(window.innerWidth / 1920, window.innerHeight / 1080);
-
-  if(!document.fullscreenElement?.contains(video) || (window.innerWidth >= 1920 && window.innerHeight >= 1080)) {
-    video.style.width = "100%";
-    video.style.height = "100%";
-    video.style.transform = "";
-    video.style.transformOrigin = "";
-    video.style.position = "";
-    video.style.top = "";
-    video.style.left = "";
-    return;
-  };
-
-  video.style.width = "1920px";
-  video.style.height = "1080px";
-  video.style.transform = `scale(${scale})`;
-  video.style.transformOrigin = "top left";
-  video.style.position = "fixed";
-  video.style.top = "0";
-  video.style.left = "0";
-});
+const link = document.createElement("link");
+link.rel = "icon";
+link.type = "image/png";
+link.href = chrome.runtime.getURL("hamivideo.webp");
+document.head.appendChild(link);
